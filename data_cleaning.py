837ab157e5ddd2e581a7efbfb66283c8c18acc7a
Connor Hardcastle <connhardcastle97@gmail.com>
2023-07-30 20:35:07 +0100

    first commit in new folder

---
+++ data_cleaning.py
@@ -0,0 +1,49 @@
+from data_extraction import DataExtractor
+from database_utils import DatabaseConnector
+import pandas as pd
+
+class DataCleaning:
+    def __init__(self):
+        de = DataExtractor()
+        self.df = de.df_rds_table
+        self.cleandata = self.clean_user_data()
+        print(self.cleandata)
+
+    def clean_user_data(self):
+        # Dropping duplicates & null values
+        cleaned_df = self.df.set_index('index')
+        # cleaned_df = cleaned_df.apply(self.remove_rows_with_numbers)
+        cleaned_df = self.df.dropna()
+        cleaned_df = cleaned_df.drop_duplicates()
+        
+        # Setting required types to category
+        cleaned_df['country'] = cleaned_df['country'].astype('category')
+        cleaned_df['country_code'] = cleaned_df['country_code'].astype('category')
+
+        # Matching country codes with countries
+        cleaned_df.loc[cleaned_df['country_code'] == 'DE', 'country'] = 'Germany'
+        cleaned_df.loc[cleaned_df['country'] == 'Germany', 'country_code'] = 'DE'
+
+        cleaned_df.loc[cleaned_df['country_code'] == 'GB', 'country'] = 'United Kingdom'
+        cleaned_df.loc[cleaned_df['country'] == 'United Kingdom', 'country_code'] = 'GB'
+
+        cleaned_df.loc[cleaned_df['country_code'] == 'US', 'country'] = 'United States'
+        cleaned_df.loc[cleaned_df['country'] == 'United States', 'country_code'] = 'US'
+
+        # Phone Number formatting done in a different function
+        # cleaned_df['phone_number'] = cleaned_df['phone_number'].apply(self.standardise_phone_number)
+
+        # Setting required columns to timedate
+        cleaned_df['date_of_birth'] = pd.to_datetime(cleaned_df['date_of_birth'], errors='coerce').dt.date
+        cleaned_df['join_date'] = pd.to_datetime(cleaned_df['join_date'], errors='coerce').dt.date
+        return cleaned_df
+
+        # .dt.date look at this. think about how to encompass on more than 1 column
+        # did I see the null values check datatypes.
+        # print(set(cleaned_df["phone_number"].head(25)))
+
+    def standardise_phone_number(self, phone_number): #check regex
+        pass
+
+if __name__ == '__main__':
+    DataCleaning()
---
+++ database_utils.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine, inspect
+import yaml
+
+class DatabaseConnector:
+    def __init__(self):
+        self.rds_dict = self.read_db_creds()
+        self.sql_dict = self.read_upload_creds()
+
+# Reading in the database credentials for the AWS database.
+    def read_db_creds(self):
+        with open('db_creds.yaml', 'r') as creds:
+            rds_dict=yaml.safe_load(creds)
+        return rds_dict
+    
+    def init_db_engine(self):     #engine creation for sqlalchemy
+        extraction_engine = create_engine(f"{'postgresql'}+{'psycopg2'}://{self.rds_dict['RDS_USER']}:{self.rds_dict['RDS_PASSWORD']}@{self.rds_dict['RDS_HOST']}:{self.rds_dict['RDS_PORT']}/{self.rds_dict['RDS_DATABASE']}")
+        return extraction_engine
+
+# Reading in credentials to upload the cleaned data to postgreSQL
+    def read_upload_creds(self):
+        with open('to_sql.yaml', 'r') as sqlcreds:
+            sql_dict=yaml.safe_load(sqlcreds)
+        return sql_dict
+    
+    def upload_to_db(self, cleandata):
+        upload_engine = create_engine(f"{'postgresql'}+{'psycopg2'}://{self.sql_dict['USER']}:{self.sql_dict['PASSWORD']}@{self.sql_dict['HOST']}:{self.sql_dict['PORT']}/{self.sql_dict['DATABASE']}")
+        dc.cleandata.to_sql('uncleaned', upload_engine)
