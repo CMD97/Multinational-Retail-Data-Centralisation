84df0d1c9180d8a2bc31f90f58e80298c6abbef3
Connor Hardcastle <connhardcastle97@gmail.com>
2023-10-24 18:37:15 +0100

    committing an updated task3

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,7 +2,6 @@ from data_extraction import DataExtractor
 from database_utils import DatabaseConnector
 import pandas as pd
 import re
-import numpy as np
 
 class DataCleaning:
     def __init__(self):
